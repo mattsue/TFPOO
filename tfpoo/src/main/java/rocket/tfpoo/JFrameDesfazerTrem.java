52736cc4a8c369f3327e94c420d409714d0610aa
e.patricio <e.patricio@w3332087.portoalegre.pucrsnet.br>
2023-11-22 12:40:33 -0300

    Ajustes JFrame

--- tfpoo/src/main/java/rocket/tfpoo/JFrameDesfazerTrem.java
+++ tfpoo/src/main/java/rocket/tfpoo/JFrameDesfazerTrem.java
@@ -122,14 +122,15 @@ public class JFrameDesfazerTrem extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void comboTrensActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboTrensActionPerformed
