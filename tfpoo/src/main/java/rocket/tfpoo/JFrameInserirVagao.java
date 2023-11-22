c19910fc894f8241f323a47529b31eb7e7d17a2a
e.patricio <e.patricio@w3332087.portoalegre.pucrsnet.br>
2023-11-22 12:40:49 -0300

    Ajustes JFrame

--- tfpoo/src/main/java/rocket/tfpoo/JFrameInserirVagao.java
+++ tfpoo/src/main/java/rocket/tfpoo/JFrameInserirVagao.java
@@ -49,7 +49,7 @@ public class JFrameInserirVagao extends javax.swing.JFrame {
         gridBagConstraints.gridheight = 2;
         gridBagConstraints.ipadx = 178;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(163, 12, 0, 110);
+        gridBagConstraints.insets = new java.awt.Insets(163, 12, 0, 130);
         jPanel1.add(comboTrem, gridBagConstraints);
 
         comboVagao.setFont(new java.awt.Font("Ebrima", 0, 18)); // NOI18N
@@ -61,7 +61,7 @@ public class JFrameInserirVagao extends javax.swing.JFrame {
         gridBagConstraints.gridheight = 2;
         gridBagConstraints.ipadx = 178;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(20, 12, 0, 110);
+        gridBagConstraints.insets = new java.awt.Insets(20, 12, 0, 130);
         jPanel1.add(comboVagao, gridBagConstraints);
 
         buttonAdicionarVagao.setBackground(new java.awt.Color(255, 153, 153));
@@ -82,7 +82,7 @@ public class JFrameInserirVagao extends javax.swing.JFrame {
         gridBagConstraints.ipadx = 69;
         gridBagConstraints.ipady = 6;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(18, 247, 168, 0);
+        gridBagConstraints.insets = new java.awt.Insets(18, 266, 168, 0);
         jPanel1.add(buttonAdicionarVagao, gridBagConstraints);
 
         labelVagao.setFont(new java.awt.Font("Ebrima", 1, 24)); // NOI18N
@@ -92,7 +92,7 @@ public class JFrameInserirVagao extends javax.swing.JFrame {
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(18, 115, 0, 0);
+        gridBagConstraints.insets = new java.awt.Insets(18, 134, 0, 0);
         jPanel1.add(labelVagao, gridBagConstraints);
 
         labelTrem.setFont(new java.awt.Font("Ebrima", 1, 24)); // NOI18N
@@ -102,16 +102,14 @@ public class JFrameInserirVagao extends javax.swing.JFrame {
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(161, 130, 0, 0);
+        gridBagConstraints.insets = new java.awt.Insets(161, 149, 0, 0);
         jPanel1.add(labelTrem, gridBagConstraints);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 16, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -119,6 +117,7 @@ public class JFrameInserirVagao extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void buttonAdicionarVagaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAdicionarVagaoActionPerformed
