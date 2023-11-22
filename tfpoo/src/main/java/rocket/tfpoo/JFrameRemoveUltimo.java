40de0cd3960d07903ee25188949ea93006114186
e.patricio <e.patricio@w3332087.portoalegre.pucrsnet.br>
2023-11-22 12:41:13 -0300

    Ajustes JFrame

--- tfpoo/src/main/java/rocket/tfpoo/JFrameRemoveUltimo.java
+++ tfpoo/src/main/java/rocket/tfpoo/JFrameRemoveUltimo.java
@@ -48,7 +48,7 @@ public class JFrameRemoveUltimo extends javax.swing.JFrame {
         gridBagConstraints.gridheight = 2;
         gridBagConstraints.ipadx = 178;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(162, 18, 0, 111);
+        gridBagConstraints.insets = new java.awt.Insets(180, 18, 0, 127);
         jPanel1.add(comboTrem, gridBagConstraints);
 
         removerUltimo.setBackground(new java.awt.Color(255, 153, 153));
@@ -69,7 +69,7 @@ public class JFrameRemoveUltimo extends javax.swing.JFrame {
         gridBagConstraints.ipadx = 42;
         gridBagConstraints.ipady = 6;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(65, 281, 166, 0);
+        gridBagConstraints.insets = new java.awt.Insets(48, 287, 165, 0);
         jPanel1.add(removerUltimo, gridBagConstraints);
 
         labelTrem.setFont(new java.awt.Font("Ebrima", 1, 24)); // NOI18N
@@ -79,16 +79,14 @@ public class JFrameRemoveUltimo extends javax.swing.JFrame {
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(160, 115, 0, 0);
+        gridBagConstraints.insets = new java.awt.Insets(178, 130, 0, 0);
         jPanel1.add(labelTrem, gridBagConstraints);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 31, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -96,6 +94,7 @@ public class JFrameRemoveUltimo extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void removerUltimoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removerUltimoActionPerformed
